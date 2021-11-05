9f20f03c5a2e535783db41e48490f4d7acc3f1e1
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-05 10:25:54 -0600

    Install and configure ng2-charts module

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ChartsModule as Ng2ChartsModule } from 'ng2-charts';
+
 import { ChartsRoutingModule } from './charts-routing.module';
 import { BarComponent } from './pages/bar/bar.component';
 import { DoubleBarComponent } from './pages/double-bar/double-bar.component';
@@ -8,18 +10,14 @@ import { DonutComponent } from './pages/donut/donut.component';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { HttpDonutComponent } from './pages/http-donut/http-donut.component';
 
-
 @NgModule({
   declarations: [
     BarComponent,
     DoubleBarComponent,
     DonutComponent,
     BarChartComponent,
-    HttpDonutComponent
+    HttpDonutComponent,
   ],
-  imports: [
-    CommonModule,
-    ChartsRoutingModule
-  ]
+  imports: [CommonModule, Ng2ChartsModule, ChartsRoutingModule],
 })
-export class ChartsModule { }
+export class ChartsModule {}
