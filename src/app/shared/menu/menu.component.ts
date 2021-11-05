dafa3229b9d3806cb003c6d1d19adc2bb43309a7
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-05 10:08:51 -0600

    Create application menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  path: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  public menuItems: MenuItem[] = [
+    { path: '/charts/bar', text: 'Bar' },
+    { path: '/charts/double-bar', text: 'Double Bar' },
+    { path: '/charts/donut', text: 'Donut' },
+    { path: '/charts/http-donut', text: 'Http Donut' },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
