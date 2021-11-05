0059f1a28f56faaaf912c22bfb45ce75526fcebb
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-05 16:41:01 -0600

    Implement double bar chart component

--- src/app/charts/components/bar-chart/bar-chart.component.ts
+++ src/app/charts/components/bar-chart/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
@@ -8,27 +8,19 @@ import { Label } from 'ng2-charts';
   templateUrl: './bar-chart.component.html',
 })
 export class BarChartComponent implements OnInit {
+  @Input() horizontal: boolean = false;
+  @Input() barChartLabels: Label[] = [];
+  @Input() barChartData: ChartDataSets[] = [];
+
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  public barChartLabels: Label[] = [
-    '2021',
-    '2022',
-    '2023',
-    '2024',
-    '2025',
-    '2026',
-    '2027',
-  ];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-  ];
-
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.barChartType = this.horizontal ? 'horizontalBar' : 'bar';
+  }
 }
