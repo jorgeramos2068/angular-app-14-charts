73a872ba771dc384eb4a062f2aff1902fb2dd6a4
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-05 09:59:36 -0600

    Create charts module and configure routes

---
+++ src/app/charts/components/bar-chart/bar-chart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styles: [
+  ]
+})
+export class BarChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/charts/pages/http-donut/http-donut.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-http-donut',
+  templateUrl: './http-donut.component.html',
+  styles: [
+  ]
+})
+export class HttpDonutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
