e49efe6c190ae923a7623b3cf17e36c32fe3cefd
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-06 10:49:07 -0600

    Create example with donut chart

--- src/app/charts/pages/donut/donut.component.ts
+++ src/app/charts/pages/donut/donut.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-donut',
   templateUrl: './donut.component.html',
-  styles: [
-  ]
 })
-export class DonutComponent implements OnInit {
+export class DonutComponent {
+  public doughnutChartLabels: Label[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: MultiDataSet = [[350, 450, 100]];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
