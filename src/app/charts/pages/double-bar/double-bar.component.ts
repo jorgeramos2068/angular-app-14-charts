255b58a075eedd818b46575620b13f0acc5f35cd
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:54:56 -0600

    Implement double bar chart component

--- src/app/charts/pages/double-bar/double-bar.component.ts
+++ src/app/charts/pages/double-bar/double-bar.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartDataSets } from 'chart.js';
 
 @Component({
   selector: 'app-double-bar',
   templateUrl: './double-bar.component.html',
-  styles: [
-  ]
 })
-export class DoubleBarComponent implements OnInit {
+export class DoubleBarComponent {
+  public suppliersLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
 
-  constructor() { }
+  public suppliersData: ChartDataSets[] = [
+    { data: [100, 200, 300, 400, 500], label: 'Vendor A' },
+    { data: [50, 250, 30, 450, 200], label: 'Vendor B' },
+  ];
 
-  ngOnInit(): void {
-  }
+  public productsData: ChartDataSets[] = [
+    {
+      data: [200, 300, 400, 300, 100],
+      label: 'Cars',
+    },
+  ];
 
+  constructor() {}
 }
